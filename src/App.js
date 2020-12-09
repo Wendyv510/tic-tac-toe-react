4162999feab97588db1055fea470a4d9968bf213
Wendy Harris <wendyv510@hotmail.com>
2020-12-09 10:28:01 -0500

    winning combinations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react' 
 import './App.css';
 
+const WIN_CONDITIONS = [
+  [0,1,2],
+  [0,4,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [3,4,5],
+  [6,7,8],
+];
+
 class App extends Component {
   constructor(props){
     super(props)
